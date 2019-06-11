31e278289a30d2862ec48f847d20afbd67ab6ac6
TheDoudou <thechikan@mac.com>
2019-06-11 03:20:22 +0200

    Add switch
    Add terrible switch :x, start mow and go to dock. (wrong folder last commit)

--- README.md
+++ README.md
@@ -1,29 +1,31 @@
-# homebridge-http-humidity
+# homebridge-boschindego
 
-Supports http/https devices on HomeBridge Platform.
-This version only supports humidity sensors returning a JSON with the data or the raw data.
+View and manage Bosch Indego Mower.
 
-This plug-in acts as an interface between a web endpoint and homebridge only. You will still need some dedicated hardware to expose the web endpoints with the humidity information. In my case, I used an Arduino board with Wifi capabilities.
+It's work :
+
+    - Auth
+    - Read state
+
+And don't work :
+
+    - Remake the code (https://github.com/nfarina/homebridge/blob/master/example-plugins/homebridge-samplePlatform/index.js)
+    - Add function with the Bosch API (https://github.com/zazaz-de/iot-device-bosch-indego-controller/blob/master/PROTOCOL.md)
 
 # Installation
 
 1. Install homebridge using: `npm install -g homebridge`
-2. Install this plugin using: `npm install -g homebridge-http-humidity`
+2. Install this plugin using: `npm install -g homebridge-boschindego`
 3. Update your configuration file. See sample-config.json in this repository for a sample.
 
 # Configuration
 
 The available fields in the config.json file are:
- - `url` [Mandatory] Endpoint URL.
- - `name` [Mandatory] Accessory name.
- - `http_method` [Optional] HTTP method used to get the humidity (Default: GET)
- - `manufacturer` [Optional] Additional information for the accessory.
- - `model` [Optional] Additional information for the accessory.
- - `serial` [Optional] Additional information for the accessory.
- - `field_name` [Optional] Field that will be used from the JSON response of the endpoint. Alternatively, if the `field_name` contains an empty string (`"field_name": ""`), the expected response is directly the current humidity value (Default: humidity).
- - `timeout` [Optional] Waiting time for the endpoint response before fail (Default: 5000ms).
- - `auth` [Optional] JSON with `user` and `pass` fields used to authenticate the request into the device.
- - `update_interval` [Optional] If not zero, the field defines the polling period in miliseconds for the sensor state (Default is 120000ms). When the value is zero, the state is only updated when homebridge requests the current value.
+ - `accessory` Accessory value.
+ - `name` Accessory name.
+ - `model` Additional information for the accessory.
+ - `email` and `pass` fields used to authenticate the request into the device.
+ - `update_interval` If not zero, the field defines the polling period in miliseconds for the sensor state (Default is 10000ms). When the value is zero, the state is only updated when homebridge requests the current value.
 
 
 Example:
@@ -31,26 +33,14 @@ Example:
  ```
  "accessories": [
      {
-         "accessory": "HttpHumidity",
-         "name": "Outside Humidity",
-         "url": "http://192.168.1.210/humidity?format=json",
-         "http_method": "GET",
-         "field_name": "humidity",
-         "auth": {
-             "user": "test",
-             "pass": "1234"
-         }
+        "accessory": "BoschIndego",
+        "name": "Mower",
+		"model": 1000,
+        "email": "test@test.com",
+		"pass": "test",
+		"update_interval": "10000",
+		"view_log": false
      }
  ]
 
 ```
-
-The defined endpoint will return a json looking like this:
-```
-{
-	"humidity": 38
-}
-```
-
-
-This plugin acts as an interface between a web endpoint and homebridge only. You will still need some dedicated hardware to expose the web endpoints with the relative humidity information. In my case, I used an Arduino board with Wifi capabilities.
--- index.js
+++ index.js
@@ -1,120 +1,271 @@
-var Service, Characteristic;
-var request = require('request');
+let request = require('request');
+require('request').debug = false
 
-const DEF_TIMEOUT = 5000,
-      DEF_INTERVAL = 120000; //120s
+let Service, Characteristic;
+//let Accessory, hap, UUIDGen;
 
 module.exports = function (homebridge) {
-   Service = homebridge.hap.Service;
-   Characteristic = homebridge.hap.Characteristic;
-   homebridge.registerAccessory("homebridge-http-humidity", "HttpHumidity", HttpHumidity);
+
+    // Accessory must be created from PlatformAccessory Constructor
+    //Accessory = homebridge.platformAccessory;
+    //hap = homebridge.hap;
+    // Service and Characteristic are from hap-nodejs
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    //UUIDGen = homebridge.hap.uuid;
+
+    // For platform plugin to be considered as dynamic platform plugin,
+    // registerPlatform(pluginName, platformName, constructor, dynamic), dynamic must be true
+    homebridge.registerAccessory("homebridge-boschindego", "BoschIndego", BoschIndego);
 }
 
+// Platform constructor
+// config may be null
+// api may be null if launched from old homebridge version
+function BoschIndego(log, config, api) {
 
-function HttpHumidity(log, config) {
-   this.log = log;
+    this.log = log;
 
-   // url info
-   this.url = config["url"];
-   this.http_method = config["http_method"] || "GET";
-   this.name = config["name"];
-   this.manufacturer = config["manufacturer"] || "@metbosch manufacturer";
-   this.model = config["model"] || "Model not available";
-   this.serial = config["serial"] || "Non-defined serial";
-   this.fieldName = config["field_name"] || "humidity";
-   this.timeout = config["timeout"] || DEF_TIMEOUT;
-   this.auth = config["auth"];
-   this.update_interval = Number( config["update_interval"] || DEF_INTERVAL );
+    this.name = config["name"];
+    this.email = config["email"];
+    this.pass = config["pass"];
+    this.model = config["model"];
+    this.auth = new Buffer(this.email + ":" + this.pass).toString("base64");
+    this.update_interval = Number(config["update_interval"] || 1200000);
 
-   // Internal variables
-   this.last_value = null;
-   this.waiting_response = false;
+    this.serial = 1;
+    this.userId = 0;
+    this.contextId = 0;
+
+    this.view_log = config["view_log"];
+    this.authed = false;
+    this.valueState = 0;
+    this.mowerState = false;
+
+    this.log("BoschIndego Init" + this.serial);
+
 }
 
-HttpHumidity.prototype = {
+BoschIndego.prototype = {
 
-   updateState: function () {
-      //Ensure previous call finished
-      if (this.waiting_response) {
-         this.log('Avoid updateState as previous response does not arrived yet');
-         return;
-      }
-      this.waiting_response = true;
-      this.last_value = new Promise((resolve, reject) => {
-         var ops = {
-            uri:    this.url,
-            method: this.http_method,
-            timeout: this.timeout
-         };
-         //this.log('Requesting humidity on "' + ops.uri + '", method ' + ops.method);
-         if (this.auth) {
-            ops.auth = {
-               user: this.auth.user,
-               pass: this.auth.pass
-            };
-         }
-         request(ops, (error, res, body) => {
-            var value = null;
-            if (error) {
-               this.log('HTTP bad response (' + ops.uri + '): ' + error.message);
+    updateState: function () {
+        //Ensure previous call finished
+        if (this.waiting_response) {
+            if (this.view_log)
+                this.log('Avoid updateState as previous response does not arrived yet');
+            return;
+        }
+        this.waiting_response = true;
+
+        if (this.authed == false)
+            this.authentication();
+
+        this.setMowerState("get");
+
+        this.log("VALUE " + this.valueState)
+
+        if (this.valueState) {
+            // Properly set the return value
+            if (this.valueState === 513 || this.valueState === 514 || this.valueState === 515 || this.valueState === 516 || this.valueState === 517 || this.valueState === 518 || this.valueState === 519 || this.valueState === 769 || this.valueState === 770 || this.valueState === 771 || this.valueState === 772 || this.valueState === 773 || this.valueState === 774 || this.valueState === 775 || this.valueState === 776) this.mowerState = true;
+            else if (this.valueState === 257 || this.valueState === 258 || this.valueState === 259 || this.valueState === 260 || this.valueState === 261 || this.valueState === 262 || this.valueState === 263) this.mowerState = false;
+
+            // Check if return value is valid
+            if (this.mowerState !== true && this.mowerState !== false) {
+                if (this.view_log)
+                    this.log('Received value is not valid. Keeping last_state: "' + this.last_state + '"');
             } else {
-               try {
-                  value = this.fieldName === '' ? body : JSON.parse(body)[this.fieldName];
-                  value = Number(value);
-                  if (value < 0 || value > 100 || isNaN(value)) {
-                     throw new Error("Invalid value received");
-                  }
-                  //this.log('HTTP successful response: ' + value);
-               } catch (parseErr) {
-                  this.log('Error processing received information: ' + parseErr.message);
-                  error = parseErr;
-               }
+                this.motionService
+                    .getCharacteristic(Characteristic.MotionDetected).updateValue(this.mowerState, null, "updateState");
+
+                this.switchService.getCharacteristic(Characteristic.On).updateValue(this.mowerState, null, "updateState");
+
+                this.last_state = this.mowerState;
+
             }
-            if (!error) {
-                resolve(value);
+        }
+        this.waiting_response = false;
+    },
+
+    getState: function (callback) {
+        let state = this.last_state;
+        let update = !this.waiting_response;
+        let sync = this.update_interval === 0;
+        if (this.view_log)
+            this.log('Call to getState: last_state is "' + state + '", will update state now "' + update + '"');
+        if (update) {
+            setImmediate(this.updateState.bind(this));
+        }
+        callback(null, state);
+    },
+    getServices: function () {
+        this.informationService = new Service.AccessoryInformation();
+        this.informationService
+            .setCharacteristic(Characteristic.Manufacturer, "Bosch")
+            .setCharacteristic(Characteristic.Model, this.model)
+            .setCharacteristic(Characteristic.SerialNumber, this.serial);
+
+        this.motionService = new Service.MotionSensor("Mowing");
+        this.motionService
+            .getCharacteristic(Characteristic.MotionDetected)
+            .on('get', this.getState.bind(this));
+
+
+
+        this.switchService = new Service.Switch("Mow/Dock");
+        this.switchService
+            .getCharacteristic(Characteristic.On)
+            .on('get', this.getState.bind(this)) //getter
+            .on('set', this.setSwitchOnCharacteristic.bind(this)); //setter
+
+        if (this.update_interval > 0) {
+            this.timer = setInterval(this.updateState.bind(this), this.update_interval);
+        }
+
+
+
+        return [this.informationService, this.motionService, this.switchService];
+    },
+    getSwitchOnCharacteristic: function (next) { //getter
+        return next(null, this.last_state);
+    },
+    setSwitchOnCharacteristic: function (on, next) { //setter
+        if (this.last_state) {
+            if (this.setMowerState("returnToDock"))
+                return next();
+        } else {
+            if (this.setMowerState("mow"))
+                return next();
+        }
+    },
+    setMowerState: function (action) {
+        let opt = {}
+
+        if (action == "get") {
+            ops = {
+                method: "GET",
+                uri: 'https://api.indego.iot.bosch-si.com/api/v1/alms/' + this.serial + '/state',
+                headers: {
+                    'x-im-context-id': ' ' + this.contextId
+                },
+                timeout: 3000
+            };
+        } else {
+            ops = {
+                method: "PUT",
+                uri: 'https://api.indego.iot.bosch-si.com/api/v1/alms/' + this.serial + '/state',
+                json: { "state": action },
+                headers: {
+                    'x-im-context-id': ' ' + this.contextId
+                },
+                timeout: 3000
+            };
+        }
+
+        //this.log('Requesting motion on "' + ops.uri + '", method ' + ops.method + ', timeout ' + ops.timeout);
+        let value = null;
+        request(ops, (error, res, body) => {
+            if (error) {
+                if (this.view_log)
+                    this.log('HTTP bad response (' + ops.uri + '): ' + error.message);
+            } else if (this.json_response === "") {
+                value = body;
+					if (this.view_log)
+                this.log('HTTP successful response: ' + body);
             } else {
-                reject(error);
+                try {
+                    value = JSON.parse(body)[this.json_response];
+                    let datatest = JSON.parse(body);
+                    this.valueState = datatest.state;
+                    if (this.view_log)
+                        this.log('HTTP successful response state code: ' + this.valueState);
+                } catch (parseErr) {
+                    if (this.view_log) {
+                        this.log('Error processing received information: ' + parseErr.message);
+                        this.log('Error processing received information: ' + res.statusCode);
+                    }
+                    error = parseErr;
+
+                    if (res.statusCode == 401)
+                        this.authed = false
+
+
+                }
             }
             this.waiting_response = false;
-         });
-      }).then((value) => {
-         this.humidityService
-            .getCharacteristic(Characteristic.CurrentRelativeHumidity).updateValue(value, null);
-         return value;
-      }, (error) => {
-         //For now, only to avoid the NodeJS warning about uncatched rejected promises
-         return error;
-      });
-   },
+        });
+    }
 
-   getState: function (callback) {
-      this.log('Call to getState: waiting_response is "' + this.waiting_response + '"' );
-      this.updateState(); //This sets the promise in last_value
-      this.last_value.then((value) => {
-         callback(null, value);
-         return value;
-      }, (error) => {
-         callback(error, null);
-         return error;
-      });
-   },
 
-   getServices: function () {
-      this.informationService = new Service.AccessoryInformation();
-      this.informationService
-      .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
-      .setCharacteristic(Characteristic.Model, this.model)
-      .setCharacteristic(Characteristic.SerialNumber, this.serial);
+}
 
-      this.humidityService = new Service.HumiditySensor(this.name);
-      this.humidityService
-         .getCharacteristic(Characteristic.CurrentRelativeHumidity)
-         .on('get', this.getState.bind(this));
+BoschIndego.prototype.authentication = function () {
+    let body = {
+        'device': '',
+        'os_type': 'Android',
+        'os_version': '4.0',
+        'dvc_manuf': 'unknown',
+        'dvc_type': 'unknown'
+    };
 
-      if (this.update_interval > 0) {
-         this.timer = setInterval(this.updateState.bind(this), this.update_interval);
-      }
+    let ops = {
+        method: 'POST',
+        uri: 'https://api.indego.iot.bosch-si.com/api/v1/authenticate',
+        body: JSON.stringify(body),
+        headers: {
+            'Authorization': 'Basic ' + this.auth,
+            'content-type': 'application/json'
+        },
+        timeout: 3000
+    };
 
-      return [this.informationService, this.humidityService];
-   }
+    request(ops, (error, res, body) => {
+        let value = null;
+        if (error) {
+            this.log('HTTP bad response (' + ops.uri + '): ' + error.message);
+        } else {
+            try {
+                value = JSON.parse(body);
+
+                this.serial = value.alm_sn;
+                this.userId = value.userId;
+                this.contextId = value.contextId;
+
+                if (this.view_log)
+                    this.log('HTTP successful LOGIN');
+                this.authed = true;
+            }
+            catch (parseErr) {
+                if (this.view_log) {
+                    this.log('Error processing received information login: ' + parseErr.message);
+                    this.log('Error processing received information login: ' + res.statusCode);
+                    this.log('Error processing received information login: ' + value);
+                }
+                error = parseErr;
+            }
+        }
+    });
 };
+
+
+/*
+Active
+CurrentPosition
+FilterLifeLevel
+FirmwareRevision
+HardwareRevision
+HoldPosition
+MotionDetected
+Identify
+InUse
+Logs
+Manufacturer
+Model
+Name
+ObstructionDetected
+On
+SerialNumber
+StatusActive
+StatusFault
+Version
+AccessoryInformation
+*/
